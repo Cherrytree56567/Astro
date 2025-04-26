7c959bcfbcb7317e0882ef5a23f32e97fc9ed27e
Cherrytree56567 <ronitdsilva82@gmail.com>
2025-04-26 13:49:22 +1000

    Scrollable Tabs

--- app/src/main/java/app/ct5/astrocam/MainActivity.kt
+++ app/src/main/java/app/ct5/astrocam/MainActivity.kt
@@ -1,49 +1,95 @@
 package app.ct5.astrocam
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
-import androidx.recyclerview.widget.RecyclerView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MyPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    // total number of pages
+    override fun getItemCount(): Int = 2
+
+    // return the Fragment associated with a given position
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> PhotoFragment()
+            else -> AstroPhotographyFragment()
+        }
+    }
+
+    private fun PhotoFragment(): Fragment {
+        TODO("Not yet implemented")
+    }
+
+    private fun AstroPhotographyFragment(): Fragment {
+        TODO("Not yet implemented")
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var modeRecyclerView: RecyclerView
-    private lateinit var modeAdapter: ModeAdapter
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val modes = listOf("Photo", "Video", "Portrait", "Panorama", "Night")
+        tabLayout = findViewById(R.id.TabLayout)
+        viewPager = findViewById(R.id.ViewPager2)
 
-        modeRecyclerView = findViewById(R.id.modeSelector)
-        modeAdapter = ModeAdapter(modes)
 
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        modeRecyclerView.layoutManager = layoutManager
-        modeRecyclerView.adapter = modeAdapter
+        val adapter = MyPagerAdapter(this)
+        viewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount() = 2
+            override fun createFragment(position: Int): Fragment {
+                return when (position) {
+                    0 -> Fragment()
+                    1 -> Fragment()
+                    else -> throw IllegalArgumentException("Invalid position")
+                }
+            }
+        }
 
-        // Snap to center
-        val snapHelper = LinearSnapHelper()
-        snapHelper.attachToRecyclerView(modeRecyclerView)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            // Set the tab text based on the position
+            tab.text = when (position) {
+                0 -> "Photo"  // Photo Tab
+                1 -> "AstroPhotography"  // Video Tab
+                else -> throw IllegalArgumentException("Invalid position")
+            }
+        }.attach()
 
-        // Detect which item is centered
-        modeRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    val centerView = snapHelper.findSnapView(layoutManager)
-                    centerView?.let {
-                        val position = layoutManager.getPosition(it)
-                        modeAdapter.setSelectedPosition(position)
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tabLayout.getTabAt(position)?.select()
+
+                tabLayout.post {
+                    val tabStrip = tabLayout.getChildAt(0) as? ViewGroup
+                    val selectedTabView = tabStrip?.getChildAt(position)
+                    selectedTabView?.let { tabView ->
+                        val scrollPos = tabView.left - (tabLayout.width - tabView.width) / 2
+                        tabLayout.smoothScrollTo(scrollPos, 0)
                     }
                 }
             }
         })
+    }
 
-        // Start centered
-        modeRecyclerView.post {
-            modeRecyclerView.scrollToPosition(modes.size / 2)
-        }
+    // Function to add a single tab at a time with just one argument (tab name)
+    private fun addTab(tabName: String) {
+        tabLayout.addTab(tabLayout.newTab().setText(tabName))
     }
 }
